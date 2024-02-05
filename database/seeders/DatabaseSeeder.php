ecb8121ffdc85ee6c9e9dd0bc8b0757b5ba4a2b0
ThijsHeuvel <d294396@edu.curio.nl>
2024-02-05 14:55:53 +0100

    Updated amount of factories ran

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
-        \App\Models\Car::factory(10)->create();
+        \App\Models\User::factory(15)->create();
+        \App\Models\Car::factory(60)->create();
     }
 }
